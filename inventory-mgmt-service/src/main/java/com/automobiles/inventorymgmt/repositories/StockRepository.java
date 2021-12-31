b0bcbca03ca6445dabd2ba96271fe5683395e0fe
rockycta <rocky.cta@gmail.com>
2021-12-31 16:05:32 +0530

    3rd setup of inventory_managment

---
+++ inventory-mgmt-service/src/main/java/com/automobiles/inventorymgmt/repositories/StockRepository.java
@@ -0,0 +1,14 @@
+package com.automobiles.inventorymgmt.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.automobiles.inventorymgmt.entites.Stock;
+
+public interface StockRepository extends JpaRepository<Stock, Integer> {
+	
+	
+	List<Stock> findByStockNameLike(String stockName);
+	
+}
